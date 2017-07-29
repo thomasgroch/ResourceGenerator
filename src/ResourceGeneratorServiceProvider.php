49141566b3ed950ccc20d168a340a8346f02d9e0
Thomas Groch <thomas.groch@gmail.com>
2017-07-29 12:52:42 -0300

    update
    installer stable

--- src/ResourceGeneratorServiceProvider.php
+++ src/ResourceGeneratorServiceProvider.php
@@ -16,12 +16,12 @@ class ResourceGeneratorServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/Console/Commands/structure_files/ApiController.php' => app_path('Http/Controllers'),
-            __DIR__.'/Console/Commands/structure_files/ApiCrudController.php' => app_path('Http/Controllers'),
-            __DIR__.'/Console/Commands/structure_files/TokenAuth' => app_path('Http/Controllers'),
-            __DIR__.'/Console/Commands/structure_files/helpers' => app_path('../tests/Unit'),
-            __DIR__.'/Console/Commands/structure_files/Transformer.php' => app_path('Transformer'),
-            __DIR__.'/Console/Commands/structure_files/CrudSave.php' => app_path('Helpers'),
+            __DIR__.'/Console/Commands/structure_files/TokenAuth/AuthenticateController.php' => app_path('Http/Controllers/TokenAuth/AuthenticateController.php'),
+            __DIR__.'/Console/Commands/structure_files/ApiController.php' => app_path('Http/Controllers/ApiController.php'),
+            __DIR__.'/Console/Commands/structure_files/ApiCrudController.php' => app_path('Http/Controllers/ApiCrudController.php'),
+            __DIR__.'/Console/Commands/structure_files/Transformer.php' => app_path('Transformers/Transformer.php'),
+            __DIR__.'/Console/Commands/structure_files/helpers' => app_path('../tests/Unit/helpers'),
+            __DIR__.'/Console/Commands/structure_files/CrudSave.php' => app_path('Helpers/CrudSave.php'),
         ], 'install-resource');
     }
 
